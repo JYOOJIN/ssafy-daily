3fe6dad0ee41858d8d4413698aa584419e10ed7a
정유진 <ellka02@naver.com>
2024-04-11 01:27:43 +0900

    0410 update

---
+++ 02_algorithm/src/daily_0409/BJ_19236_청소년상어2.java
@@ -0,0 +1,27 @@
+package daily_0409;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_19236_청소년상어2 {
+	
+	
+	static class Fish{
+		int d,num;
+		boolean isShark, isLive;
+		public Fish(int d, int num, boolean isShark, boolean isLive) {
+			super();
+			this.d = d;
+			this.num = num;
+			this.isShark = isShark;
+			this.isLive = isLive;
+		}
+		
+	}
+
+	public static void main(String[] args) throws Exception{
+		
+		
+	}
+
+}
